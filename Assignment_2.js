bee37d9d419b66388840c384d4437a816994eca4
smb301293 <47523184+smb301293@users.noreply.github.com>
2026-04-29 08:44:12 +0530

    Add files via upload

---
+++ Assignment_2.js
@@ -0,0 +1,24 @@
+function isOddOrEven (number) 
+{
+   if (number%2)
+   {
+    console.log("Odd number is",number);
+   } else {
+    console.log("Even number is",number);
+   }
+}
+isOddOrEven ("125455");
+isOddOrEven ("125");
+isOddOrEven ("1000000000");
+isOddOrEven ("1000000001");
+
+   
+function isOddOrEven () {
+for (let x=50;x<65;x++)
+   if (x%2 === 0) {
+      console.log("Even number is",x);
+   } else if (x%2 != 0) {
+      console.log("odd number is",x);
+   }
+}
+isOddOrEven ()
