9bc73759b20cf6652b712cf20d4088328645280a
smb301293 <47523184+smb301293@users.noreply.github.com>
2026-04-29 08:41:36 +0530

    Add files via upload

---
+++ Assignment_1.js
@@ -0,0 +1,27 @@
+let firstName = 'RAVI';
+console.log(firstName);
+console.log(typeof(firstName));
+let companyName = 'BGI';
+console.log(companyName);
+console.log(typeof(companyName));
+let mobileNumber = 994403163;
+console.log(mobileNumber);
+console.log(typeof(mobileNumber));
+let isAutomation = true;
+console.log(isAutomation);
+console.log(typeof(isAutomation));
+console.log(typeof(hasPlaywright));
+
+
+
+var num = 10;
+console.log(typeof(num));
+var name = 'RAVI';
+console.log(typeof(name));
+var isCertified = true;
+console.log(typeof(isCertified));
+var date = '26-04-2026';
+console.log(typeof(date));
+var Status = null;
+console.log(typeof(Status));
+console.log(typeof(update));
